6b043bf3214241c6c0e762754bc59f49ff904d7f
Korshunov Egor <egor.dezreng@gmail.com>
2021-08-28 16:42:37 +0300

    feat: add solution

---
+++ product-servise/src/functions/getProductsList/handler.ts
@@ -0,0 +1,17 @@
+import 'source-map-support/register';
+
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
+import { formatJSONResponse } from '@libs/apiGateway';
+import { middyfy } from '@libs/lambda';
+
+const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async (_event) => {
+	try {
+		const mock = await require('@libs/db/mock.json');
+
+  	return formatJSONResponse(mock.body, 200);
+	} catch(err) {
+		return formatJSONResponse({ message: err }, 404);
+	}
+}
+
+export const main = middyfy(getProductsList);
