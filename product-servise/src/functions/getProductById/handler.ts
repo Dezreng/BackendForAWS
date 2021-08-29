65ac15cad0dd7b1e65975b8ba5c367ad97a337d4
Korshunov Egor <egor.dezreng@gmail.com>
2021-08-29 17:29:38 +0300

    feat: add unit tests

--- product-servise/src/functions/getProductById/handler.ts
+++ product-servise/src/functions/getProductById/handler.ts
@@ -1,19 +1,19 @@
 import 'source-map-support/register';
 
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
-import { formatJSONResponse } from '@libs/apiGateway';
-import { middyfy } from '@libs/lambda';
+import { formatJSONResponse } from '../../libs/apiGateway';
+import { middyfy } from '../../libs/lambda';
 
-const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async (event) => {
+export const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async (event) => {
 	try {
-		const mock = require('@libs/db/mock.json');
+		const mock = require('../../libs/db/mock.json');
 		const product = await mock.body.filter(element => {
 			if (element.id === event.pathParameters.productId){
 				return element;
 			}
 		});
 		if (product.length < 1) {
-			return formatJSONResponse({ messege: "Not Found Product" }, 404);
+			return formatJSONResponse({ message: "Not Found Product" }, 404);
 		} else {
 			return formatJSONResponse(product, 200);
 		}
--- product-servise/src/functions/getProductsList/handler.ts
+++ product-servise/src/functions/getProductsList/handler.ts
@@ -1,13 +1,12 @@
 import 'source-map-support/register';
 
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
-import { formatJSONResponse } from '@libs/apiGateway';
-import { middyfy } from '@libs/lambda';
+import { formatJSONResponse } from '../../libs/apiGateway';
+import { middyfy } from '../../libs/lambda';
 
-const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async (_event) => {
+export const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof Object> = async (_event) => {
 	try {
-		const mock = await require('@libs/db/mock.json');
-
+		const mock = await require('../../libs/db/mock.json');
   	return formatJSONResponse(mock.body, 200);
 	} catch(err) {
 		return formatJSONResponse({ message: err }, 404);
