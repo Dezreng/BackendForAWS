e6884e3dab970cc4ebcd659fb6b36188b247d877
Korshunov Egor <egor.dezreng@gmail.com>
2021-10-03 22:53:52 +0300

    feat: add new change

--- product-servise/serverless.ts
+++ product-servise/serverless.ts
@@ -25,11 +25,63 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+			SNS_ARN: "SNSTopic",
     },
     lambdaHashingVersion: '20201221',
+			iamRoleStatements: [
+			{
+    		Effect: 'Allow',
+    		Action: ['sqs:*'],
+    		Resource: "${cf:import-service-dev.SQSQueueArn}",
+  		},
+			{
+    		Effect: 'Allow',
+    		Action: ['sns:*'],
+    		Resource: "SNSTopic",
+  		}
+		]
   },
   // import the function via paths
   functions: { getProductsList, getProductById, postNewProduct },
+	resources: {
+    Resources: {
+      SNSTopic: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketName: 'AWS::SNS::Topic',
+          Properties: {
+						TopicName: "createProductTopic"
+					}
+        },
+      },
+			SNSSubscription: {
+				Type: "AWS::SNS::Subscription",
+				Properties: {
+					Endpoint: "egor.layk@gmail.com",
+					Protocol: "email",
+					TopicArn: {
+						Ref: "SNSTopic"
+					},
+					FilterPolicy: {
+						price: "lessThousand"
+					}
+				}
+			},
+			SNSSubscription2: {
+				Type: "AWS::SNS::Subscription",
+				Properties:{
+					Endpoint: "egor.dezreng@gmail.com",
+					Protocol: "email",
+					TopicArn: {
+						Ref: "SNSTopic"
+					},
+					FilterPolicy: {
+						price: "moreThousand"
+					}
+				}
+			}
+    },
+  },
 };
 
 module.exports = serverlessConfiguration;
